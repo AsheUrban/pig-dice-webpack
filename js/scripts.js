2609c23bd5aa4901cbf452fe0e2aca8367d8658d
Ashe Urban <ashe@goldentongue.com>
2022-06-15 14:59:09 -0700

    CLEAN scripts doc to function Player and function rollDice.
    Co-authored-by: Manuel Raygoza <manny.raygoza@gmail.com>
    > > Co-authored-by: DonovanWeber <donovanweber03@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,10 @@
-// Business Logic for ScoreCard ------
-
-function ScoreCard () {
-  this.players = {};
-  this.currentId = 0;
-}
-
-ScoreCard.prototype.addPlayer = function(player) {
-  player.id = this.assignId();
-  this.players[player.id] = player;
-};
-
-ScoreCard.prototype.assignId = function() {
-  this.currentId += 1; //should this just asign two ids?
-  return this.currentId;
-};
-
-ScoreCard.prototype.findPlayer = function(id) {
-  if (this.players[id] != undefined) {
-    return this.players[id];
-  }
-  return false;
-}
-
-// ScoreCard.prototype.deletePlayer = function(id) {
-//   if (this.players[id] === undefined) {
-//     return false;
-//   }
-//   delete this.players[id];
-//   return true; 
-// };
 
 // Business Logic for Player ------
 
-function Player(firstName, totalScore) {
-  this.firstName = firstName;
-  this.totalScore = totalScore;
+function Player() {
+  this.score = 0;
+  this.lastroll2 = 0;
 }
-
-// Player.prototype.update = function() {
-//   return this.totalScore + currentScore;
-// }
 
 // Business Logic for Dice
 
@@ -48,16 +13,3 @@ function rollDice() {
 } 
 // rollDice(); = 1-6// this is currentScore
 
-// UI Logic ------
-
-let scoreCard = new ScoreCard();
-
-function showScore(playerId) {
-  const player = ScoreCard.findPlayer(playerId);
-  $("#show-score").show();
-  $("#btn").click(function(){
-    
-  });
-}
-
-$('#displayRollDice').empty();
