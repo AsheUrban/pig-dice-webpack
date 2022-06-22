d69434c277b67a72b8226fc0091003ffd72001ab
Ashe Urban <ashe@goldentongue.com>
2022-06-22 11:42:19 -0700

    ADD first test.
    Co-authored-by: PJ Perry <pjperry28@gmail.com>

---
+++ _tests_/scripts.test.js
@@ -0,0 +1,20 @@
+import { TestScheduler } from 'jest';
+import Player from './../src/scripts.js';
+
+describe('Player', () => {
+
+  test("should correctly create a player object with three variables", () =>  {
+    const player = new Player(0,0,0);
+    expect (player.totalScore).toEqual(0);
+    expect (player.roundScore).toEqual(0);
+    expect (player.currentScore).toEqual(0);
+})
+
+
+
+  // test('should generate a random number between 1 and 6', () => {
+  //   const newPlayer = new Player(5)
+  //   expect(newPlayer.rollDice()).toEqual(number);
+  // });
+  
+});
